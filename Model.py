51cc8e8f146c4aebce8b61ba350d26b3b083cea1
r0gu3c0de <148647205+r0gu3c0de@users.noreply.github.com>
2023-10-21 11:19:26 -0600

    Add files via upload

--- Model.py
+++ Model.py
@@ -1,71 +1,78 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import Dense
-import keras as kr
+from prophet import Prophet
 
-# import the data frame (csv)
-def import_data(file, encode):
-    return pd.read_csv(file, encoding = encode)
 
-#Clean data by eliminating unnecessary columns such as name, email, age and country
-def clean_data(df, tables_to_drop, output_column):
-    df_clean = df.drop(tables_to_drop, axis = 1)
-    df_output = df[output_column]
-    return df_clean, df_output
-
-#Normalize the input values with numbers between 0-1
-def normalize_data(df_input, df_output):
-    scaler = MinMaxScaler()
-    df_input_normalized = scaler.fit_transform(df_input)
-    df_output = df_output.values.reshape(-1,1) 
-    df_output_normalized = scaler.fit_transform(df_output)
-    return df_input_normalized, df_output_normalized
-
-def train_data(df_input, df_output, dim, plot):
-    #Dividing the data into training/test
-    df_input_train, df_input_test, df_output_train, df_output_test = train_test_split(df_input, df_output, test_size=.2)
-
-    #Create the model
-    model = Sequential()
-    model.add(Dense(25, input_dim = dim, activation = 'relu'))
-    model.add(Dense(25, activation = 'relu'))
-    model.add(Dense(1, activation = 'linear'))
-
-    #Training the model
-    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
-    history = model.fit(df_input_train, df_output_train, epochs=100, batch_size = 25, verbose = 1, validation_split = 0.2)
-
-    #Graph of performance of our model
-    if plot:
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
-        plt.title('Model lost progress during training')
-        plt.ylabel('Training and validation loss')
-        plt.xlabel('Epoch number')
-        plt.legend(['Training loss', 'Validation loss'])
-        plt.show()
-
-    return model
-
-def training(file, encode):
-    data_clients = import_data(file, encode)
-    data_clients_clean, data_clients_output = clean_data(data_clients,['Customer Name', 'Customer e-mail', 'Country', 'Car Purchase Amount'], 'Car Purchase Amount')
-    data_clients_clean_normalized, data_clients_output_normalized = normalize_data(data_clients_clean, data_clients_output)
-    return train_data(data_clients_clean_normalized, data_clients_output_normalized, len(data_clients_clean.columns), True)
+def load_csv(file):
+    return pd.read_csv(file, error_bad_lines = False)
 
 def main():
-    trained_model = training('Car_Purchasing_Data.csv','ISO-8859-1')
-    client = np.array([[1,35,60000,10000,600000]])
-    prediction = trained_model.predict(client)
-    print('Purchase amount of this customer is: ',prediction,'$')
-    #Errases the saved model
-    kr.backend.clear_session()
+    chicago_df_1 = load_csv("Chicago_Crimes_2001_to_2004.csv")
+    chicago_df_2 = load_csv("Chicago_Crimes_2005_to_2007.csv")
+    chicago_df_3 = load_csv("Chicago_Crimes_2008_to_2011.csv")
+    chicago_df_4 = load_csv("Chicago_Crimes_2012_to_2017.csv")
+    chicago_df_total = pd.concat([chicago_df_1, chicago_df_2, chicago_df_3, chicago_df_4])
 
-if __name__ == '__main__':
+    chicago_df_total.drop(['Unnamed: 0', 'Case Number', 'ID', 'IUCR', 'X Coordinate',
+    'Y Coordinate', 'Updated On', 'Year', 'FBI Code', 'Beat', 'Ward', 
+    'Community Area', 'Location', 'District', 'Latitude', 'Longitude'], inplace=True, axis=1)
+
+    chicago_df_total.Date = pd.to_datetime(chicago_df_total.Date, format= '%m/%d/%Y %I:%M:%S %p')
+    #Converts the date into the indexes
+    chicago_df_total.index = pd.DatetimeIndex(chicago_df_total.Date)
+
+    chicago_df_total['Primary Type'].value_counts()
+    order_data = chicago_df_total['Primary Type'].value_counts().iloc[:15].index
+
+    # plt.figure(figsize = (15,10))
+    # sns.countplot(y = 'Primary Type', data = chicago_df_total, order = order_data)
+
+    # plt.figure(figsize=(15,10))
+    # sns.countplot(y='Location Description', data=chicago_df_total, order=chicago_df_total['Location Description'].value_counts().iloc[:15].index)
+    # plt.show()
+
+    #Gives us the number of crimes per year
+    # plt.plot(chicago_df_total.resample('Y').size())
+    # plt.title('Crime Count Per Year')
+    # plt.xlabel('Years')
+    # plt.ylabel('Number of Crimes')
+
+    # #Gives us the number of crimes per month 
+    # plt.plot(chicago_df_total.resample('M').size())
+    # plt.title('Crime Count Per Month')
+    # plt.xlabel('Months')
+    # plt.ylabel('Number of Crimes')
+
+    # #Gives us the number of crimes per quarter
+    # plt.plot(chicago_df_total.resample('Q').size())
+    # plt.title('Crime Count Per Quarter')
+    # plt.xlabel('Quarter')
+    # plt.ylabel('Number of Crimes')
+    # plt.show()
+
+    #We transform the data so that we have the month as date 
+    #and in another column we have the number of crimes of that month
+    chicago_prophet = chicago_df_total.resample('M').size().reset_index()
+
+    #Rename columns
+    chicago_prophet.columns = ['Date', 'Crime Count']
+
+    #We change the name again of the columns so that we can use facebook prophet
+    chicago_prophet_final = chicago_prophet.rename(columns = {'Date': 'ds', 'Crime Count': 'y'})
+
+    #We assign our data to our "prophet"
+    model = Prophet()
+    model.fit(chicago_prophet_final)
+    #Start predicting :)
+    future = model.make_future_dataframe(periods = 365)
+    forecast = model.predict(future)
+    #print(forecast)
+    model.plot(forecast, xlabel='Date', ylabel='Crime Rate')
+    #with .plot_components(forecast) we can predict the seasonal trend
+    plt.show()
+
+
+if '__main__' == __name__:
     main()
-
-
