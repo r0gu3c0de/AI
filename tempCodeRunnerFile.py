180032b2e07a98cd36df476d3b8c25da1527fbcd
r0gu3c0de <148647205+r0gu3c0de@users.noreply.github.com>
2023-10-21 11:25:37 -0600

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-def train_data(df_input, df_output):
-#     df_input_train, df_input_test, df_output_train, df_output_test = train_test_split(df_input, df_output, test_size=.2)
-#     model = Sequential()
-#     model.add(Dense(25, input_dim = 5, activation = 'relu'))
-#     model.add(Dense(25, activation = 'relu'))
