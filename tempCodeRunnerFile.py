40242b833c516650e2a3072aa720eda685c7ece5
r0gu3c0de <148647205+r0gu3c0de@users.noreply.github.com>
2023-10-21 11:15:39 -0600

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+def train_data(df_input, df_output):
+#     df_input_train, df_input_test, df_output_train, df_output_test = train_test_split(df_input, df_output, test_size=.2)
+#     model = Sequential()
+#     model.add(Dense(25, input_dim = 5, activation = 'relu'))
+#     model.add(Dense(25, activation = 'relu'))
